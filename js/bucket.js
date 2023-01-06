159a9915d4368a0260df0297f3999d0485d562e1
BisiDavi <oludavidconnect@gmail.com>
2023-01-06 13:02:41 +0100

    bucket updated

--- js/bucket.js
+++ js/bucket.js
@@ -1,58 +1,111 @@
-$(function () {
-  // used for accessing the GCS JSON API.
-  const GCS_BASE_URL = "https://www.googleapis.com/storage/v1";
+// used for accessing the GCS JSON API.
+const GCS_BASE_URL = "https://www.googleapis.com/storage/v1";
 
-  let metadata_xhr = null;
-
-  /**
-   * Forms a URL to the GCS JSON API given a path and an optional list of query
-   * parameters to include.
-   */
-  function make_gcs_url(path, params) {
-    let query_string = "?key=" + GCS_API_KEY;
-    if (params != undefined) {
-      for (prop in params) {
-        query_string += "&" + prop + "=" + params[prop];
-      }
+/**
+ * Forms a URL to the GCS JSON API given a path and an optional list of query
+ * parameters to include.
+ */
+function make_gcs_url(path, params) {
+  let query_string = "?key=" + GCS_API_KEY;
+  if (params != undefined) {
+    for (prop in params) {
+      query_string += "&" + prop + "=" + params[prop];
     }
-    return GCS_BASE_URL + path + query_string;
   }
+  return GCS_BASE_URL + path + query_string;
+}
 
-  function fetch_data() {
-    let metadata_url;
-    if (metadata_xhr !== null) {
-      metadata_xhr.abort();
-    }
-    metadata_url = make_gcs_url("/b/" + GCS_BUCKET + "/o/", { alt: "json" });
-    metadata_xhr = $.ajax({
-      url: metadata_url,
-      dataType: "json",
-      success: function (data) {
-        const mediaFiles = data.items.filter(
-          (item) => item.contentType !== "text/plain"
-        );
-        let dataArray = [];
-        mediaFiles.map((item) => {
-          const name = item.name.includes("audios")
-            ? item.name.split("/audios/")[1]
-            : item.name.split("/videos/")[1];
-          const category = item.name.includes("audios")
-            ? item.name.split("/audios/")[0]
-            : item.name.split("/videos/")[0];
-          const itemId = item.id.split(`/${item.generation}`)[0];
-          const itemUrl = `https://storage.googleapis.com/${itemId}`;
-          dataArray.push({
-            name,
-            category,
-            itemUrl,
-            contentType: item.contentType,
-            bucket: item.bucket,
-          });
-        });
-        console.log("dataArray", dataArray);
-      },
+function formatData(data) {
+  const dataArray = [];
+  const mediaFiles = data.filter((item) => item.contentType !== "text/plain");
+  mediaFiles.map((item) => {
+    const name = item.name.includes("audios")
+      ? item.name.split("/audios/")[1]
+      : item.name.split("/videos/")[1];
+    const category = item.name.includes("audios")
+      ? item.name.split("/audios/")[0]
+      : item.name.split("/videos/")[0];
+    const itemId = item.id.split(`/${item.generation}`)[0];
+    const url = `https://storage.googleapis.com/${itemId}`;
+    dataArray.push({
+      name,
+      category,
+      url,
+      type: item.contentType,
+      bucket: item.bucket,
     });
+  });
+  console.log("dataArray", dataArray);
+  return dataArray;
+}
+
+function fetch_data() {
+  const metadata_url = make_gcs_url("/b/" + GCS_BUCKET + "/o/", {
+    alt: "json",
+  });
+  return $.ajax({
+    url: metadata_url,
+    dataType: "json",
+  });
+}
+
+function assignMedia(media) {
+  const { url, type, name, category } = media;
+  const audio = document.getElementById("audio");
+  const video = document.getElementById("video");
+
+  document.getElementById("title").innerHTML = name;
+  let queryParams = new URL(location.href);
+  queryParams.searchParams.set("category", category);
+  history.pushState(null, "", queryParams);
+
+  if (COUNT === 1) {
+    audio.play();
   }
 
-  fetch_data();
-});
+  if (type.includes("video")) {
+    document.getElementById("audio").classList.add("hide");
+    document.getElementById("video").classList.remove("hide");
+    video.src = url;
+    audio.pause();
+    video.play();
+  } else {
+    document.getElementById("video").classList.add("hide");
+    document.getElementById("audio").classList.remove("hide");
+    audio.src = url;
+    audio.play();
+    video.pause();
+  }
+}
+
+const data = fetch_data();
+data.then((response) => console.log("response", response));
+
+let COUNT = 0;
+
+async function previousMedia() {
+  if (COUNT > 0) {
+    const dataResult = await data;
+    const result = formatData(dataResult.items);
+    COUNT -= 1;
+    const media = result[COUNT];
+    assignMedia(media);
+  }
+}
+
+async function nextMedia() {
+  const dataResult = await data;
+  const result = formatData(dataResult.items);
+  COUNT += 1;
+  const media = result[COUNT];
+  assignMedia(media);
+}
+
+if (COUNT === 0) {
+  COUNT = 0;
+  data.then((response) => {
+    const result = formatData(response.items);
+    const media = result[COUNT];
+    assignMedia(media);
+  });
+}
